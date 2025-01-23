cbe26c78a92079f53756cbfd7b88f3f6e78839b5
Angelo Brown <130711987+angelob120@users.noreply.github.com>
2025-01-23 03:49:14 -0500

    Resolved merge conflicts and co

---
+++ css & js/HVAC/script.js
@@ -0,0 +1,6 @@
+// IMPORTANT: If the JavaScript is not working it is most probably because it relies on external libraries that are linked in the index.html file - however, if you don't have the permission to access them, it's better not to link them.
+
+// Please keep in mind that JS files might also be obfuscated to prevent people from stealing the code and animations.
+
+// However, all the HTML elements and CSS are there are you just need to access them using JS.
+
